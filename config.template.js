5dce5b393b9a74f0b0057ceef29c46428de35bfc
Tristan <87515991+neilalcabasa@users.noreply.github.com>
2026-01-29 00:10:39 +0800

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -4,10 +4,11 @@
 const CONFIG = {
     // Replace with your Google Apps Script Web App URL
     // Get this from: Google Sheet > Extensions > Apps Script > Deploy > Web App URL
-    GOOGLE_SHEET_URL: 'YOUR_WEB_APP_URL_HERE',
+    GOOGLE_SHEET_URL: 'https://script.google.com/macros/s/AKfycbxG6lGszAHb4C9ajwnMH9ZUC2f2fqFnFUswn-b0cNZfPOH-vmQN1INU92fHOXA0cFMDlQ/exec
+',
     
     // Change this to your desired admin password
-    ADMIN_PASSWORD: 'your_secure_password_here'
+    ADMIN_PASSWORD: 'damnson2026'
 };
 
 // Make config available globally
